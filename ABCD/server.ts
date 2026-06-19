c98b3e09cf235e47500a0d55a046139568a7c30d
thamaraiselvang2025aids-oss <thamaraiselvan.g2025aids-oss@sece.ac.in>
2026-06-20 01:19:46 +0530

    Fix Railway port

--- ABCD/server.ts
+++ ABCD/server.ts
@@ -485,7 +485,7 @@ async function startServer() {
     });
     app.use(vite.middlewares);
   } else {
-    const distPath = path.join(process.cwd(), "dist");
+    const distPath = path.resolve(process.cwd(), "dist");
     app.use(express.static(distPath));
     app.get("*", (req, res) => {
       res.sendFile(path.join(distPath, "index.html"));
